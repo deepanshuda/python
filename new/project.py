ef9552af49c9b06bcbcf06adece955f46ffd5115
deepanshuda <deepanshu.da@gmail.com>
2018-06-11 17:15:09 +0530

    added data loop

--- new/project.py
+++ new/project.py
@@ -19,7 +19,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('\nHello! Let\'s explore some US bikeshare data!\n')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
         city = input("Enter the City for which you want to see Data - Chicago, New York City, Washington:\n")
@@ -200,26 +200,19 @@ def user_stats(df, month, day):
 
 
 def main():
-    # while True:
-    #     city, month, day = get_filters()
-    #     df = load_data(city, month, day)
-    #
-    #     time_stats(df)
-    #     station_stats(df)
-    #     trip_duration_stats(df)
-    #     user_stats(df)
-    #
-    #     restart = input('\nWould you like to restart? Enter yes or no.\n')
-    #     if restart.lower() != 'yes':
-    #         break
-    city, month, day = get_filters()
-    df = load_data(city, month, day)
-    time_stats(df)
-    station_stats(df)
-    trip_duration_stats(df)
-    user_stats(df, month, day)
-    # print("\n")
-    # print(df.head())
+    while True:
+        city, month, day = get_filters()
+        df = load_data(city, month, day)
+        time_stats(df)
+        station_stats(df)
+        trip_duration_stats(df)
+        user_stats(df, month, day)
+        # print("\n")
+        # print(df.head())
+
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
 
 
 if __name__ == "__main__":
