02fa685f2c6174d83500a22c1c02cebd1b794278
deepanshuda <deepanshu.da@gmail.com>
2018-06-12 17:43:42 +0530

    modifying data as per month and day

--- new/project.py
+++ new/project.py
@@ -85,39 +85,43 @@ def load_data(city, month, day):
     df['hour'] = df['Start Time'].dt.hour
 
     # filter by month if applicable
-    # if month != 'all':
-    #     # filter by month to create the new dataframe
-    #     month = months.index(month) + 1
-    #
-    #     df = df[df['month'] == month]
-    #
-    # # filter by day of week if applicable
-    # if day != 'all':
-    #     # use the index of the months list to get the corresponding int
-    #     # days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
-    #
-    #     # filter by day of week to create the new dataframe
-    #     day = int(day) - 1
-    #
-    #     df = df[df['day_of_week'] == day]
+    if month != 'all':
+        # filter by month to create the new dataframe
+        month = months.index(month) + 1
+
+        df = df[df['month'] == month]
+
+    # filter by day of week if applicable
+    if day != 'all':
+        # use the index of the months list to get the corresponding int
+        # days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+
+        # filter by day of week to create the new dataframe
+        day = int(day) - 1
+
+        df = df[df['day_of_week'] == day]
 
     return df
 
 
-def time_stats(df):
+def time_stats(df, month, day):
     """Displays statistics on the most frequent times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
 
     # display the most common month
-    # common_month = df['month'].value_counts().idxmax() - 1
-    common_month = df['month'].mode()[0] - 1
-    print("\nMost common month is {}\n".format(months[common_month]))
+    if month is 'all':
+        common_month = df['month'].mode()[0] - 1
+        print("\nMost common month is {}\n".format(months[common_month]))
 
     # display the most common day of week
-    common_day = df['day_of_week'].value_counts().idxmax()
-    print("\nMost common day of week is {} : {}\n".format(common_day, days[common_day]))
+    if day == 'all':
+        common_day = df['day_of_week'].value_counts().idxmax()
+        if month is 'all':
+            print("\nMost common day of week is {}\n".format(days[common_day]))
+        else:
+            print("\nMost common day of week in {} is {}\n".format(month, days[common_day]))
 
     # display the most common start hour
     common_hour = df['hour'].value_counts().idxmax()
@@ -219,17 +223,69 @@ def user_stats(df, month, day):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-' * 40)
 
+def user_stats_new(df, month, day):
+    """Displays statistics on bikeshare users."""
+
+    print('\nCalculating User Stats...\n')
+    start_time = time.time()
+
+    # Display counts of user types
+    if month != 'all' and day != 'all':
+        month_value = months.index(month) + 1
+        day_value = int(day) - 1
+        users_month = df.loc[df['month'] == month_value]
+        user_types = users_month.loc[users_month['day_of_week'] == day_value, 'User Type'].value_counts()
+        print("\nVarious types of users for {} in {} are:\n{}".format(days[day_value], month, user_types))
+    elif month != 'all':
+        month_value = months.index(month) + 1
+        user_types = df.loc[df['month'] == month_value, 'User Type'].value_counts()
+        print("\nVarious types of users for {} are:\n{}".format(month, user_types))
+    elif day != 'all':
+        day_value = int(day) - 1
+        user_types = df.loc[df['day_of_week'] == day_value, 'User Type'].value_counts()
+        print("\nVarious types of users for {} are:\n{}".format(days[day_value], user_types))
+    else:
+        user_types = df['User Type'].value_counts()
+        print("\nVarious types of users are:\n{}".format(user_types))
+
+    # Display counts of gender
+    if 'Gender' in df.columns:
+        gender_count = df['Gender'].value_counts()
+        gender_type = input("\nWhich gender statistics would you prefer - male, female or both?\n")
+        if gender_type.lower() == 'male':
+            print("\nMales: {}\n".format(gender_count['Male']))
+        elif gender_type.lower() == 'female':
+            print("\nFemales: {}\n".format(gender_count['Female']))
+        elif gender_type.lower() == 'both':
+            print("\nMales, Females: {}, {}\n".format(gender_count['Male'], gender_count['Female']))
+    else:
+        print("\nNo data available for Gender\n")
+
+    # Display earliest, most recent, and most common year of birth
+    if 'Birth Year' in df.columns:
+        earliest_year = df['Birth Year'].min()
+        recent_year = df['Birth Year'].max()
+        common_year = df['Birth Year'].value_counts().idxmax()
+        print("\nEarliest Year: {}\n".format(earliest_year))
+        print("\nRecent Year: {}\n".format(recent_year))
+        print("\nCommon Year: {}\n".format(common_year))
+    else:
+        print("\nNo data available for Birth Year\n")
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-' * 40)
+
 
 def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-        time_stats(df)
-        station_stats(df)
-        trip_duration_stats(df)
-        user_stats(df, month, day)
         # print("\n")
         # print(df.head())
+        time_stats(df, month, day)
+        # station_stats(df)
+        # trip_duration_stats(df)
+        # user_stats(df, month, day)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
